c59805817a914f3b0d0f9709a50016190d79b669
Uday Kiran Dunna <dunna.udaykiran1729@gmail.com>
2025-05-04 13:57:56 +0530

    Update main.jsx

--- cravito_frontend/src/main.jsx
+++ cravito_frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from "./App.jsx";
 import {BrowserRouter} from "react-router-dom"
-import Context from './context/context.jsx';
+import Context from './context/Context.jsx';
 
 
 
